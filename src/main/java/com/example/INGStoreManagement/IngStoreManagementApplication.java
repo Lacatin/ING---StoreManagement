367da01458383f40b909ceb1f9f962828773b4e1
Lacatin <florin_catalin.cismaru@yahoo.com>
2025-08-31 18:49:28 +0300

    Init project

---
+++ src/main/java/com/example/INGStoreManagement/IngStoreManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.INGStoreManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngStoreManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngStoreManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/INGStoreManagement/IngStoreManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.INGStoreManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngStoreManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
